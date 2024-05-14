b745f71fcc7c1e4184fef470d9beabc86c500153
Victor Biasibetti <victorbiasibetti7@gmail.com>
2024-05-14 17:29:57 -0300

    create post item to show comments of post

--- src/components/Post/PostItem.tsx
+++ src/components/Post/PostItem.tsx
@@ -2,8 +2,22 @@ import { Post } from "./types";
 
 export const PostItem = (post: Post) => {
   return (
-    <div>
-      <p>{post.title}</p>
+    <div
+      style={{
+        marginBottom: "1rem",
+        cursor: "pointer",
+      }}
+      onClick={() => console.log("postId - " + post.id)}
+    >
+      <p
+        style={{
+          textTransform: "uppercase",
+          fontWeight: 700,
+          paddingBottom: "0.5rem",
+        }}
+      >
+        {post.title}
+      </p>
       <span>{post.body}</span>
     </div>
   );
