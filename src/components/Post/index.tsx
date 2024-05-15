c9509f7f91f22cb76303269a0bbef54fff05c258
Victor Biasibetti <victorbiasibetti7@gmail.com>
2024-05-15 09:48:42 -0300

    add filter to posts

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,20 +1,42 @@
 import { Post as PostType } from "@/types/Post";
 import { PostItem } from "./PostItem";
 import { User } from "@/types/User";
+import { useState } from "react";
 
 type Props = { posts: PostType[]; users: User[] };
 
 const Post = ({ posts, users }: Props) => {
+  const [filterPost, setFilterPost] = useState<string>("");
+
+  const postsWithUsersInfo = posts.map((post) => ({
+    ...post,
+    user: users.find((user) => user.id == post.userId),
+  }));
+
   return (
     <div>
-      {posts &&
-        posts.map((post) => (
-          <PostItem
-            key={post.id}
-            post={post}
-            user={users.find((user) => user.id == post.userId)}
-          />
-        ))}
+      <div>
+        <h4>Filter post:</h4>
+        <input
+          value={filterPost}
+          onChange={(e) => setFilterPost(e.target.value)}
+        />
+      </div>
+      {postsWithUsersInfo &&
+        postsWithUsersInfo
+          .filter(
+            (post) =>
+              post.userId.toString() === filterPost ||
+              post.body
+                .toLocaleLowerCase()
+                .includes(filterPost.toLocaleLowerCase()) ||
+              post.user?.username
+                .toLocaleLowerCase()
+                .startsWith(filterPost.toLocaleLowerCase())
+          )
+          .map((post) => (
+            <PostItem key={post.id} post={post} user={post.user} />
+          ))}
     </div>
   );
 };
