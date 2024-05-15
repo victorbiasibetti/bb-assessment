f8b9f9ab62a98a8f8812b5e288c1ac965312a0ac
Victor Biasibetti <victorbiasibetti7@gmail.com>
2024-05-15 08:06:26 -0300

    save replies on redux

--- src/redux/reducers/comment.ts
+++ src/redux/reducers/comment.ts
@@ -1,22 +1,33 @@
 import { Comment } from "@/types/Comment";
 import { createSlice } from "@reduxjs/toolkit";
 
-type Reply = { userId: number; body: string };
-type Tag = string;
-type CommentSlice = Comment & Reply[] & Tag[];
+type Reply = {
+  body: string;
+  postId: number;
+  commentId: number;
+};
+type Tag = {
+  userId: number;
+  body: string;
+  postId: number;
+  commentId: number;
+};
 
-const comments: Array<CommentSlice> = [];
+const comments: Array<Comment> = [];
 const tags: Array<Tag> = [];
+const replies: Array<Reply> = [];
 
 export const commentSlice = createSlice({
   name: "comment",
   initialState: {
     comments,
+    replies,
     tags,
   },
   reducers: {
-    addReply: () => {
-      console.log("comment/addReply reducer");
+    addReply: (state, action: { type: string; payload: Reply }) => {
+      const newReply = action.payload;
+      state.replies.push({ ...newReply });
     },
     addTagToComment: () => {
       console.log("comment/addTagToComment reducer");
@@ -25,12 +36,12 @@ export const commentSlice = createSlice({
       console.log("comment/getTags reducer");
     },
     loadComment: (state, action) => {
-      state.comments = action.payload?.comments as CommentSlice[];
+      state.comments = action.payload?.comments as Comment[];
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { loadComment } = commentSlice.actions;
+export const { addReply, loadComment } = commentSlice.actions;
 
 export default commentSlice.reducer;
